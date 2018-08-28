fcfc3f878a74bb3ee8d80ab1ad334d557ba12591
pkpusa <prakashpusa@gmail.com>
2018-08-28 07:04:22 +0530

    slq file added

--- usersignup.php
+++ usersignup.php
@@ -2,22 +2,8 @@
 session_start();
 
 ?>
-
-
-<!--A Design by W3layouts
-Author: W3layout
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-
-
-
 <!DOCTYPE HTML>
 <html>
-    
-    
-
 <head>
 	<title>Medikit User Registration</title>
 	<!-- Meta tags -->
